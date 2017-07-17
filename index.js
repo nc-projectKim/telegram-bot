6d6cf7f5d06c34bab55e36c99fcea39bdf558f7c
Adie Williams <hello@adie.codes>
2017-07-17 15:57:23 +0100

    Add responses to 'hello' and 'bye'

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const { token } = require('./config.js');
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
-  const Hi = 'hi';
-  if (msg.text.toLowerCase().indexOf(Hi) === 0) {
+  const start = ['hi', 'hello'];
+  if (start.indexOf(msg.text.toLowerCase()) !== -1) {
     bot.sendMessage(msg.chat.id, 'Hello dear user');
+  }
+  const bye = 'bye';
+  if (msg.text.toLowerCase().indexOf(bye) === 0) {
+    bot.sendMessage(msg.chat.id, 'See you soon!');
   }
 });
