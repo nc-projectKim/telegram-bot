0044e97a4e02028bb693cb6840d3b4fd6a3c4fcf
Adie Williams <hello@adie.codes>
2017-07-18 17:30:19 +0100

    Create, get and view notes - Feat 1 (#3)
    ## Features
    
    - /note creates new note through a form.
    - /get lists all the note titles with index position.
    - /view allows you to view full note via index
    
    ## Commits
    
    * Add responses to 'hello' and 'bye'
    
    * Add response to '/start'
    
    * Provide custom keyboard after '/start'
    
    * Add telegram-node-bot in place of telegram-node-bot api
    
    * Replace airbnb eslint with custom rules
    
    * Add notes via /note command
    
    * Add fallback path for unknown queries
    
    * Return list of notes using /get command
    
    * Now return list of all notes, this fixes #2
    
    * Add persistant storage for user and chat
    
    * Add form to /note command
    
    * Change /get to display note header and body
    
    * Add /view command to see individual note body
    
    * fix: Fixed small bug with user data being passed outside of obj

---
+++ src/controllers/NoteController.js
@@ -0,0 +1,82 @@
+const Telegram = require('telegram-node-bot');
+
+const noteForm = {
+    title: {
+        q: 'What title do you want to give your note?',
+        error: 'sorry, need text',
+        validator: (message, callback) => {
+            if (message.text) {
+                callback(true, message.text); // you must pass the result also
+                return;
+            }
+
+            callback(false);
+        }
+    },
+    body: {
+        q: 'What do you want to remember?',
+        error: 'sorry, wrong input',
+        validator: (message, callback) => {
+            if (message.text) {
+                callback(true, message.text);
+                return;
+            }
+
+            callback(false);
+        }
+    }
+};
+
+class NoteController extends Telegram.TelegramBaseController {
+    noteHandler($) {
+        $.runForm(noteForm, (result) => {
+          const header = result.title;
+          const body = result.body;
+          $.getUserSession('notes')
+            .then(notes => {
+              if (!Array.isArray(notes)) {
+                  $.setUserSession('notes', [{header, body}]);
+              } else {
+                  $.setUserSession('notes', notes.concat([{header, body}]));
+              }
+              $.sendMessage('Added new note!');
+            });
+        });
+    }
+
+    getHandler($) {
+      $.getUserSession('notes')
+        .then(notes => {
+          $.sendMessage(this._serializeList(notes), {parse_mode: 'Markdown'});
+        });
+    }
+
+    viewHandler($) {
+      let index = parseInt($.message.text.split(' ').slice(1)[0]);
+      if (isNaN(index)) return $.sendMessage('Sorry, you didn\'t pass a valid index.');
+
+      $.getUserSession('notes')
+        .then(notes => {
+          if (index >= notes.length) return $.sendMessage('Sorry, you didn\'t pass a valid index.');
+          $.sendMessage(`*${notes[index].header}*\n\n${notes[index].body}`, {parse_mode: 'Markdown'});
+        });
+    }
+
+    get routes() {
+        return {
+            noteCommand: 'noteHandler',
+            getCommand: 'getHandler',
+            viewCommand: 'viewHandler'
+        };
+    }
+
+    _serializeList(notesList) {
+      let serialized = '*Your Notes:*\n\n';
+      notesList.forEach((note, i) => {
+        serialized += `*${i}* - *${note.header}*\n\n`;
+      });
+      return serialized;
+    }
+}
+
+module.exports = NoteController;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+// // TODO: Add cancel option part way through input
+
+const Telegram = require('telegram-node-bot');
+const { token } = require('../config');
+const PersistentMemoryStorage = require('./adaptors/PersistentMemoryStorage');
+const NoteController = require('./controllers/NoteController');
+const OtherwiseController = require('./controllers/OtherwiseController');
+
+const storage = new PersistentMemoryStorage(
+  `${__dirname}/data/userStorage.json`,
+  `${__dirname}/data/chatStorage.json`
+);
+
+// initialise bot
+const tg = new Telegram.Telegram(token, {
+  workers: 1,
+  storage,
+});
+
+const noteCtrl = new NoteController();
+
+tg.router.when(new Telegram.TextCommand('/note', 'noteCommand'), noteCtrl)
+  .when(new Telegram.TextCommand('/get', 'getCommand'), noteCtrl)
+  .when(new Telegram.TextCommand('/view', 'viewCommand'), noteCtrl)
+  .otherwise(new OtherwiseController());
+
+function exitHandler(exitCode) {
+  storage.flush();
+  process.exit(exitCode);
+}
+
+process.on('SIGINT', exitHandler.bind(null, 0));
+process.on('uncaughtException', exitHandler.bind(null, 1));
