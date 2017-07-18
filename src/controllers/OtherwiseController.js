dd0f67016396f44b93ae37453c8708675094afe5
Adie Williams <hello@adie.codes>
2017-07-18 15:24:21 +0100

    Add fallback path for unknown queries

---
+++ src/controllers/OtherwiseController.js
@@ -0,0 +1,9 @@
+const Telegram = require('telegram-node-bot');
+
+class OtherwiseController extends Telegram.TelegramBaseController {
+    handle($) {
+        $.sendMessage('Sorry, I don\'t understand');
+    }
+}
+
+module.exports = OtherwiseController;
